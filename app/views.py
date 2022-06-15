02ccaab5a9bc2931cfaf5ec0d85b56879c3cab07
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-15 08:09:35 +0300

    st:views-configurations - configures for teplate rendering

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+#home function
+def home(request):
+    return render (request, 'index.html')
+
+#Register function
+def register(request):
+    return render (request, 'register.html')
+
+
+#Login function
+def user_login(request):
+    return render (request, 'login.html')
+
