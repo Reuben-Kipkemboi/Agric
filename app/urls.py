0c9b2466021a69932e615504c54313e270b696fb
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-20 18:45:35 +0300

    st:register - update register form

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    # path('login/', views.user_login, name='login'),
+    
 ]
