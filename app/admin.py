8d02fcf6449559a9b611e3179af4116b27363ae6
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-23 15:15:17 +0300

    st:button-styling - optimize button styling

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-# admin.site.register(User)
-# admin.site.register(Public)
-# admin.site.register(Owner)
+admin.site.register(Feedback)
+admin.site.register(Machinery)
+admin.site.register(Order)
 admin.site.register(Profile)
