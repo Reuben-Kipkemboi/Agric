920f2303e5640681435ed24f1d5e3b1fc374b2a0
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2023-03-11 10:36:47 +0300

    st:form-input-styling - update input styling

--- app/forms.py
+++ app/forms.py
@@ -29,7 +29,7 @@ class LoginForm(forms.Form):
     username = forms.CharField(
         widget= forms.TextInput(
             attrs={
-                "class": "form-control"
+                "class": "form-control", "style":'height:5vh;'
             }
         )
     )
@@ -37,7 +37,7 @@ class LoginForm(forms.Form):
         widget=forms.PasswordInput(
             attrs={
                 "class": "form-control",'data-toggle': 'password',
-                 'id': 'password',
+                 'id': 'password',"style":'height:5vh;'
             }
         )
     )
