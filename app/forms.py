927602c58f0b232a9abc1341c9528d4a31aa524b
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2023-03-26 10:34:08 +0300

    st:card-styling - optimize card-styling

--- app/forms.py
+++ app/forms.py
@@ -2,42 +2,48 @@ from django import forms
 from . models import *
 from django.contrib.auth.forms import UserCreationForm
 
+
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
-        
+
 #machinery update form
+
+
 class MachineryUpdateForm(forms.ModelForm):
     class Meta:
         model = Machinery
         exclude = ['owner_id']
-     
+
 #comments form
+
+
 class CommentForm(forms.ModelForm):
-     class Meta:
+    class Meta:
         model = Feedback
-        fields=['user_name','email', 'content', 'rating']
-        
+        fields = ['user_name', 'email', 'content', 'rating']
+
+
 class OrderForm(forms.ModelForm):
-     class Meta:
+    class Meta:
         model = Order
-        exclude=['user_id','machinery_id']
-        
-        
+        exclude = ['user_id', 'machinery_id']
+
+
 class LoginForm(forms.Form):
     username = forms.CharField(
-        widget= forms.TextInput(
+        widget=forms.TextInput(
             attrs={
-                "class": "form-control", "style":'height:5vh;'
+                "class": "form-control", "style": 'height:5vh;'
             }
         )
     )
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "class": "form-control",'data-toggle': 'password',
-                 'id': 'password',"style":'height:5vh;'
+                "class": "form-control", 'data-toggle': 'password',
+                'id': 'password', "style": 'height:5vh;'
             }
         )
     )
@@ -47,35 +53,36 @@ class SignUpForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "class": "form-control","style":'height:4.5vh;'
+                "class": "form-control", "style": 'height:4.5vh;'
             }
         )
     )
     password1 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "class": "form-control",'data-toggle': 'password',
-                 'id': 'password',"style":'height:4.5vh;'
+                "class": "form-control", 'data-toggle': 'password',
+                'id': 'password', "style": 'height:4.5vh;'
             }
         )
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "class": "form-control",'data-toggle': 'password',
-                 'id': 'password',"style":'height:4.5vh;'
-                
+                "class": "form-control", 'data-toggle': 'password',
+                'id': 'password', "style": 'height:4.5vh;'
+
             }
         )
     )
     email = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "class": "form-control","style":'height:4.5vh;'
+                "class": "form-control", "style": 'height:4.5vh;'
             }
         )
     )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', 'is_admin', 'is_owner', 'is_public')
+        fields = ('username', 'email', 'password1', 'password2',
+                  'is_admin', 'is_owner', 'is_public')
